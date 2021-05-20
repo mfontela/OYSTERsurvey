b5ac0bed2b32d56f0b5fa53fab50053af1093832
mfontela <marcosmfontela@gmail.com>
2021-05-20 18:31:47 +0200

    update masterscript

--- OYSTERsurvey_masterscript.R
+++ OYSTERsurvey_masterscript.R
@@ -3,3 +3,8 @@ source("01_load.R") #load packages and data (data not available online for data 
 source("02_plot_theme.R") #common plot theme
 source("03_basic_manipulations.R") #some basic transformations of data
 
+#Individual scripts for plots
+source("Q16plot.R")
+source("Q20plot.R")
+source("Q28plot.R")
+source("Q14vQ28_analysis.R")
