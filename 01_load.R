c3b52e371ffe1882c3412057f2981f617d5f52eb
mfontela <marcosmfontela@gmail.com>
2021-11-17 12:52:29 +0100

    Data availability

--- 01_load.R
+++ 01_load.R
@@ -11,7 +11,13 @@ library(scales)
 #load data
 
 D <- read_excel("MASTER copy survey2020 covid19.xlsx", 
-                sheet = "Quantitative coded data") #DATA available upon reasonable request
+                sheet = "Quantitative coded data")
+# NOTE: The raw data generated for this study cannot be publicly available due to 
+# restrictions imposed by ethics requirements and agreements with research 
+# participants [providing such data would compromise the participants]. 
+# Selected data that does not breach any ethics requirements are available 
+# on request from the corresponding author upon reasonable request
+
 #Convert character to numeric
 D<-mutate_if(D, is.character,as.numeric)
 
