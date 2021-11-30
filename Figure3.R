c10df73ba3014d50f8c4ff91290f66d75a249e14
mfontela <marcosmfontela@gmail.com>
2021-11-30 23:08:51 +0100

    update Figure 3

--- Figure3.R
+++ Figure3.R
@@ -5,8 +5,6 @@
 #' while ECRs who highly depended on lab- or field-work 
 #' to acquire data felt significantly more pressure
 #' 
-#' @Marcos, do you think it's worth to do a graph showing these 
-#' results? I think this comes from analysing Q18 vs 12b 
 #' 
 # Q12_b	Which location is the most important for performing your job?
 # 0	Field
@@ -56,7 +54,8 @@ D$location=NA
 D$location[D$Q12_b==2]="Desk"
 D$location[D$Q12_b!=2]="Not-desk"
 
-ggplot(D, aes(y=Q18_v2, x=location, colour=Location))+
+print(
+  ggplot(D, aes(y=Q18_v2, x=location, colour=Location))+
   facet_wrap(. ~ stage)+
   geom_boxplot(size=1.4, outlier.shape = 17, outlier.size = 2.5)+
   geom_jitter(size=1, alpha=.6, aes(colour=Location))+
@@ -72,6 +71,7 @@ ggplot(D, aes(y=Q18_v2, x=location, colour=Location))+
         panel.grid.major.x = element_line(colour="gray", size=1,linetype=2))+
   coord_flip()+
   scale_color_manual(values=OYSTERpalette4)
+)
 
 save_svg=0
 if (save_svg) {
